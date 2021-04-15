61cda4faaa902ca3640d4cea83ab3625414de5ae
kkyu <gusrb8925@naver.com>
2021-04-15 11:21:28 +0900

    21-04-15 몽고디비 멜론차트 크롤링

---
+++ SpringPRJ3/src/poly/persistance/mongo/IMelonMapper.java
@@ -0,0 +1,23 @@
+package poly.persistance.mongo;
+
+import java.util.List;
+
+import poly.dto.MelonDTO;
+
+public interface IMelonMapper {
+
+    /**
+     * MongoDB 컬렉션 생성하기
+     *
+     * @param colNm 생성하는 컬렉션 이름
+     */
+    public boolean createCollection(String colNm) throws Exception;
+
+    /**
+     * MongoDB 데이터 저장하기
+     *
+     * @param pDTO 저장될 정보
+     */
+    public int insertRank(List<MelonDTO> pList, String colNm) throws Exception;
+
+}
---
+++ SpringPRJ3/src/poly/service/IMelonService.java
@@ -0,0 +1,14 @@
+package poly.service;
+
+import java.util.List;
+
+import poly.dto.MelonDTO;
+
+public interface IMelonService {
+
+    /**
+     * 멜론 Top100 순위 수집하기
+     */
+    public int collectMelonRank() throws Exception;
+
+}
