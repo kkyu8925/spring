a7d9fd8bce0e6bc7b3484ed0430ff83d6ee52c3c
kkyu <gusrb8925@naver.com>
2021-06-02 15:19:50 +0900

    21-06-02 section1

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/HelloWorldController.java
@@ -0,0 +1,27 @@
+package com.example.restfulwebservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 자동으로 json 반환 @Controller + @ResponseBody
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld() {
+
+        return "hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+
+        return new HelloWorldBean("hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name) {
+
+        return new HelloWorldBean("hello World, " + name);
+    }
+}
