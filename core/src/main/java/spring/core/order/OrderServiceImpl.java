372c1ad8ac37770c2b532f95d252dbd3706daa1f
kkyu <gusrb8925@naver.com>
2021-06-09 14:46:39 +0900

    21-06-09 스프링 핵심 원리 이해2 - 객체 지향 원리 적용

--- core/src/main/java/spring/core/order/OrderServiceImpl.java
+++ core/src/main/java/spring/core/order/OrderServiceImpl.java
@@ -1,15 +1,23 @@
 package spring.core.order;
 
 import spring.core.discount.DiscountPolicy;
-import spring.core.discount.FixDiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
-import spring.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    // 생성자 주입
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
