83cb686582bcdc5a532871430631433ab2d4be5d
gusrb8925 <gusrb8925@naver.com>
2021-07-10 23:11:08 +0900

    21-07-10 review

--- woowahan/core/src/main/java/spring/core/member/MemberServiceImpl.java
+++ woowahan/core/src/main/java/spring/core/member/MemberServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class MemberServiceImpl implements MemberService {
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // DIP 위반, 역할과 구현에 의존하고 있음.
 
     // 생성자 주입
     private final MemberRepository memberRepository;
@@ -27,7 +28,7 @@ public class MemberServiceImpl implements MemberService {
     }
 
     // 테스트 용도
-   public MemberRepository getMemberRepository() {
+    public MemberRepository getMemberRepository() {
         return memberRepository;
     }
 }
