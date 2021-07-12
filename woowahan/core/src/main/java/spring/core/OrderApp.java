c768020a7e065d87706878fa08852e325c4326e2
gusrb8925 <gusrb8925@naver.com>
2021-07-12 21:39:26 +0900

    21-07-12 review

--- woowahan/core/src/main/java/spring/core/OrderApp.java
+++ woowahan/core/src/main/java/spring/core/OrderApp.java
@@ -26,6 +26,8 @@ public class OrderApp {
 //        ApplicationContext 은 BeanFactory 기능을 모두 상속받아서 제공한다.
 //        빈을 관리하고 조회하는 기능은 말고 다른 수 많은 부가기능을 제공한다.
 
+//        BeanFactory, ApplicationContext 을 스프링 컨테이너라 한다.
+        
 //        3. 스프링 컨테이너로 생성자 주입
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
