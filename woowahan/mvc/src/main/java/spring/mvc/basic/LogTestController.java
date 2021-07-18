4ff86984d1f60d0743df8dcf5e7ff3b3b6a23d03
gusrb8925 <gusrb8925@naver.com>
2021-07-18 16:44:13 +0900

    21-07-17 review

--- woowahan/mvc/src/main/java/spring/mvc/basic/LogTestController.java
+++ woowahan/mvc/src/main/java/spring/mvc/basic/LogTestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+// @slf4j = private final Logger log = LoggerFactory.getLogger(getClass());
 @RestController
 public class LogTestController {
 
@@ -19,8 +20,11 @@ public class LogTestController {
         System.out.println("name = " + name);
 
         // 이 방식은 연산이 일어난다.
-       log.trace(" trace my log="+ name);
+        // 연산이 일어나면서 리소스 낭비
+        log.trace(" trace my log=" + name);
 
+        // 올바른 사용법
+        // 파라미터로 넘기면서 출력이 일어나지 않는다.
         log.trace("trace log={}", name);
         log.debug("debug log={}", name);
         log.info(" info log={}", name);
